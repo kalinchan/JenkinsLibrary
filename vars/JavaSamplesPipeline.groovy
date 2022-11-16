dea631e31fb49846e3e630502042ce7554a5a6f8
Kalin Chan <kalin.chan@payara.fish>
2022-11-16 12:38:39 +0000

    implementing docker building

--- vars/JavaSamplesPipeline.groovy
+++ vars/JavaSamplesPipeline.groovy
@@ -18,7 +18,7 @@ def call(body) {
 
     pipeline {
         node {
-            git url: MPL.config.'git.url', branch: MPL.config.'git.branch'
+            git url: MPL.config.'git.url', branch: MPL.config.'git.branch', credentialsId: MPL.config.git.credentials
             def myEnv = docker.build 'java-samples:snapshot'
             mvEnv.inside{
                 sh """mvn clean install"""
