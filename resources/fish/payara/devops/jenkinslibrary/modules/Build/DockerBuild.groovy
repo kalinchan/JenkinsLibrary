60cab1a963772af9843c431fb7b0e5d5ad4006dc
Kalin Chan <kalin.chan@payara.fish>
2022-11-16 17:14:48 +0000

    test

--- resources/fish/payara/devops/jenkinslibrary/modules/Build/DockerBuild.groovy
+++ resources/fish/payara/devops/jenkinslibrary/modules/Build/DockerBuild.groovy
@@ -1,5 +1,7 @@
 #! groovy
 docker.withTool('docker') {
+    sh """apt-get update"""
+    sh """apt-get install sudo"""
     sh """sudo chmod 666 /var/run/docker.sock"""
     sh """docker run hello-world"""
 }
