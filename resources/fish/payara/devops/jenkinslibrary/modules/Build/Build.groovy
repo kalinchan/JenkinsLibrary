ff716c4395c90578e658db4d49a8e529a82e654a
Kalin Chan <kalin.chan@payara.fish>
2022-11-16 15:34:20 +0000

    fix errors

--- resources/fish/payara/devops/jenkinslibrary/modules/Build/Build.groovy
+++ resources/fish/payara/devops/jenkinslibrary/modules/Build/Build.groovy
@@ -1,7 +1,11 @@
 #! groovy
 if ( fileExists('Dockerfile') )
+    echo "Dockerfile found"
     MPLModule('Docker Build', CFG)
 else 
     echo "No Dockerfile found in "
 if ( fileExists('pom.xml') )
-    MPLModule('Maven Build', CFG)    
+    echo "Pom file found"
+    MPLModule('Maven Build', CFG)
+else
+    echo "No maven file found"
--- resources/fish/payara/devops/jenkinslibrary/modules/Build/DockerBuild.groovy
+++ resources/fish/payara/devops/jenkinslibrary/modules/Build/DockerBuild.groovy
@@ -1,4 +1,4 @@
 #! groovy
-withTool(docker){
+docker(docker){
     sh """docker build ."""
 }
